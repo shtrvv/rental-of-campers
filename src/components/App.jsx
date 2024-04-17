94785e4b7fa3e5c28ba711e9562744931926e7db
shtrvv <shatrova.liza.21@gmail.com>
2024-04-17 23:02:32 +0300

    initial setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
-export const App = () => {
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <p>hello</p>
+    </>
   );
 };
+
+export default App;

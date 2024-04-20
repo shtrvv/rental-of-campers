45610e790415ab70ae1e8024e592e7400dcd0729
shtrvv <133063494+shtrvv@users.noreply.github.com>
2024-04-20 01:14:41 +0000

    Deploying to gh-pages from @ shtrvv/rental-of-campers@e3ec176be473f0c2ff2980dff1d5b91b76d9919a 🚀

---
+++ static/js/413.b9815547.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[413],{4413:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("p",{children:"Favorite"})})}}}]);
+//# sourceMappingURL=413.b9815547.chunk.js.map
